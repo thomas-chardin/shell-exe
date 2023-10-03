9455709c016e8972bcbc0d90da37247b0c76115e
thomas-chardin <thomas.chardin@laplateforme.io>
2023-10-03 17:11:22 +0200

    mise a jour

--- Job06/my_calculator.sh
+++ Job06/my_calculator.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 nombre1=$1
 nombre2=$3
 symbole=$2
 
+if [ $symbole="x" ]
+then
+symbole=*
+fi
 resultat=$(($nombre1 $symbole $nombre2))
 
 echo "result: $resultat"
---
+++ Job07/myloop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Je suis un script qui arrive à faire une boucle $1”
