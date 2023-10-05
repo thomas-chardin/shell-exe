8f56a55fcbab9ff4447d96340613b5e6f897ce18
thomas-chardin <thomas.chardin@laplateforme.io>
2023-10-05 15:50:25 +0200

    mise à jour

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+!/bin/bash
 
-dateDuJour=$(date +"%Y%m%d%H%M")
-logConnexion="/var/log/auth.log"
-fichier="number_connection-$dateDuJour"
+date_actuelle=$(date +"%d-%m-%Y-%H:%M")
+logfile="/var/log/auth.log"
+connexions=$(sudo grep "session open" "$logfile")
+nombre_connexions=$(echo "$connexions" | wc -l)
 
-connections=$(sudo grep "session opened for user thomas" $logConnexion)
-nb_lignes=$(echo "$connections" | wc -l)
+fin_fichier="number_connection-$date_actuelle"
+nom_fichier="/home/thomas/Documents/shell-exe/Job08/number_connection-$date_actuelle"
 
-echo "$nb_lignes" > /home/thomas/Documents/shell-exe/Job08/$fichier 
-
-
-#tar -cf /home/thomas/Documents/shell-exe/Jobs08/$fichier.tar /home/thomas/Documents/shell-exe/Jobs08/$fichier
-#mv /home/thomas/Documents/shell-exe/Jobs08/$fichier.tar /home/thomas/Documents/shell-exe/Jobs08/Backups
-
-#rm /home/thomas/Documents/shell-exe/Jobs08/$fichier
+echo "$nombre_connexions" > "$nom_fichier"
+tar -cvf /home/thomas/Documents/shell-exe/Job08/Backup/$fin_fichier.tar $nom_fichier
+sudo rm /home/thomas/Documents/shell-exe/Job08/$nom_fichier
